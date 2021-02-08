fe715e484a92d2c4c7596561207db26c89ab89a6
Dip Roy <diproy5632@gmail.com>
2021-02-08 10:25:25 +0600

    lab task 2

---
+++ LAb_task_1_4_sln/LAb_task_4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+namespace lab_task_1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int i = 0;
+            Console.WriteLine("Even Numbers :");
+            for (i = 1; i <= 50; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            int j, k, sum, esum = 0, osum = 0;
+
+            for (j = 2; j <= 100; j += 2)
+            {
+                esum += i;
+            }
+            Console.WriteLine("\n Sum of all even number between 1 to 100 = " + esum);
+
+
+
+            for (k = 1; i <= 100; i += 2)
+            {
+                osum += i;
+            }
+            Console.WriteLine("Sum of all odd numbers between 1 to 100 = " + osum);
+
+            sum = esum + osum;
+            Console.WriteLine("Sum of all even and odd number between 1 to 100 = " + sum);
+        }
+    }
+}
---
+++ Lab_task_8_sln/Lab_task_8/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Marksheet1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r, s1, s2, s3, s4, s5, t;
+            Double p;
+           
+            Console.WriteLine("Enter N5umber \n");
+            Console.WriteLine("Mark of Physics : ");
+            s1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mark of Chemistry: ");
+            s2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mark of Biology : ");
+            s3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mark of Math : ");
+            s4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mark of Computer : ");
+            s5 = Convert.ToInt32(Console.ReadLine());
+            t = s1 + s2 + s3 + s4 + s5 ;
+            p = t / 5.0f;
+            
+            Console.WriteLine("Percentage : " + p);
+            if (p >= 0 && p < 50)
+                 {
+                     Console.WriteLine("Grade is F");
+                 }
+            if (p >= 50 && p < 74)
+                 {
+                     Console.WriteLine("Grade is C+");
+                 }
+            if (p >= 75 && p <= 79)
+                 {
+                      Console.WriteLine("Grade is B");
+                 }
+            if (p > 80 && p <= 84)
+                 {
+                      Console.WriteLine("Grade is B+");
+                 }
+            if (p > 85 && p <= 89)
+                 {
+                       Console.WriteLine("Grade is A");
+                  }
+            if (p > 90 && p <= 100)
+                  {
+                       Console.WriteLine("Grade is A+");
+                 }
+            Console.ReadLine();
+        }
+    }
+}
