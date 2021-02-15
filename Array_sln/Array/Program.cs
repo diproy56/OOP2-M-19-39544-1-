572c8880e30e94e46e20cf90c34ebe97408d548d
Dip Roy <diproy5632@gmail.com>
2021-02-15 11:04:15 +0600

    Lab task 4

---
+++ Array_sln/Array/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5 };
+            int[] arr2= new int[] {6, 7, 8 ,9 ,0};
+            int count;
+
+            Console.WriteLine("Original array: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Array in reverse order: ");
+             
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i] + " ");
+            } 
+
+            {
+                var sum = 0;
+                for (var i = 0; i < arr.Length; i++)
+                {
+                    sum += arr[i];
+                }
+                Console.WriteLine("\nArray Sum: " + sum);
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = arr2[i];
+            }
+            for(int k=0; k< arr2.Length;k++)
+            {
+                Console.WriteLine(arr2[k] + "");
+            }
+            Console.WriteLine();
+
+            for (int j = 0; j < arr.Length - 1; j++)
+            {
+
+                if (arr[j] == arr[j + 1])
+                    count = count + 1;
+            }
+            Console.WriteLine("\t\n " + arr[i] + "occurse" + count);
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
+
+
+
+
+
---
+++ TASK2_sln/TASK2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK2
+{
+    class Program
+                {
+                    static void Main(string[] args)
+                    {
+                        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+                     Console.WriteLine(string.Join(",", nums));
+                     var sum = 0;
+                        for (var i = 0; i < nums.Length; i++)
+                        {
+                            sum += nums[i];
+                        }
+                       Console.WriteLine("Sum: "+ sum);
+                } 
+            }
+        }
