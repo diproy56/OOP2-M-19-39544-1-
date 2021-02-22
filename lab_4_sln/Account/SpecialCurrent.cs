0258ec7fb51fb235002d1690d9b0692394fbc5dd
Dip Roy <diproy5632@gmail.com>
2021-02-22 10:57:08 +0600

    lab task 4

---
+++ lab_4_sln/Account/SpecialCurrent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account
+{
+    class SpecialCurrent
+    {
+        public SavingsAccount(string o, double ir): 
+     base(o, 0.0M, ir) {
+   }
+
+   public SavingsAccount(string o, decimal b, double ir): 
+
