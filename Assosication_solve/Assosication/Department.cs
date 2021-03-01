799de2c24401c72e4d7dfc04ca8d6d25c751d087
Dip Roy <diproy5632@gmail.com>
2021-03-01 12:13:33 +0600

    lab task 6

---
+++ Assosication_solve/Assosication/Department.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assosication
+{
+    class Department
+    {
+        private string id;
+        private string name;
+
+        private int studentCount;
+        private student[] students;
+
+        public Department()
+        {
+            students = new student[60];
+        }
+
+        public Department(string id, string name)
+        {
+            this.Name = name;
+            this.Id = id;
+            students = new student[60];
+        }
+
+        public string Id
+        {
+            set { this.id = value; }
+            get { return this.id; }
+        }
+
+        public string Name
+        {
+            set { this.name = value; }
+            get { return this.name; }
+        }
+
+        public int StudentCount
+        {
+            set { this.studentCount = value; }
+            get { return this.studentCount; }
+        }
+
+        public void AddStudent(params student[] stdns)
+        {
+            foreach (student s in stdns)
+            {
+                this.students[this.StudentCount++] = s;
+                s.Dept = this;
+            }
+        }
+
+
+
+        public student GetStudent(string id)
+        {
+
+            return null;
+        }
+
+        public void PrintStudent()
+        {
+            for (int i = 0; i < studentCount; i++)
+            {
+                students[i].ShowInfo();
+            }
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Department Name: {0}", this.Name);
+            Console.WriteLine("Department Id: {0}", this.Id);
+        }
+    }
+}
---
+++ Assosication_solve/Assosication/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assosication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student s1 = new student("Roy", "12345", 2.1f);
+            student s2 = new student("Dip", "54321", 2.5f);
+            student s3 = new student("joy", "78901", 3.1f);
+
+            Department d1 = new Department("Computer science", "CS");
+            Department d2 = new Department("English", "English");
+            s1.Dept = d1;
+            d1.AddStudent(s1, s2, s3);
+            d1.AddStudent(s2);
+            s2.Dept = d2;
+            s3.Dept = d2;
+            d2.AddStudent(s3);
+            s1.ShowInfo();
+            s3.ShowInfo();
+            Console.WriteLine("***************");
+            d1.Name = "Computer Science and Engineering";
+            d1.Id = "CSE";
+            s2.ShowSudentDeatils();
+            Console.WriteLine("***************");
+            d2.PrintStudent();
+            s3.Name = "MR.X";
+            d2.PrintStudent();
+
+        }
+    }
+}
